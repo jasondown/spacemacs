9d3c3dfb14678d16aafb7e43fb857a4269a4e6c1
Down <Down_J@homehardware.ca>
2016-07-29 10:36:04 -0400

    Added themes-megapack

--- init.el
+++ init.el
@@ -39,6 +39,7 @@ values."
      ;; spell-checking
      smex
      syntax-checking
+     themes-megapack
      unimpaired
      version-control
      )
@@ -101,13 +102,9 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(solarized-dark
-                         spacemacs-dark
-                         spacemacs-light
-                         solarized-light
-                         leuven
-                         monokai
-                         zenburn)
+   dotspacemacs-themes '(sanityinc-tomorrow-eighties
+                         sanityinc-solarized-dark
+                         spacemacs-dark)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font. `powerline-scale' allows to quickly tweak the mode-line
